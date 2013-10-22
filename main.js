e52032575e89c685c4401b10b18b1a114e77a174
Evan Hahn <me@evanhahn.com>
2013-10-22 11:06:36 -0400

    basic functionality complete

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ $(document).ready(function() {
 	var $answerForm = $('#answer-form');
 	var $answerInput = $('#answer-input');
 
+	var rightAnswer;
+
 	function newQuestion() {
+		var number = random();
+		rightAnswer = getKorean(number);
+		$question.html(number);
+		$answerInput.val('').focus();
 	}
+
+	$answerInput.on('keyup', function() {
+		var yourAnswer = $.trim($answerInput.val());
+		if (yourAnswer === rightAnswer)
+			newQuestion();
+	});
 
 	newQuestion();
 
