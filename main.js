41385d673cadb65991ec27c1a4e083c6722fd421
Evan Hahn <me@evanhahn.com>
2013-10-22 10:47:12 -0400

    add skeleton UI

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	var $body = $(document.body);
+	var $question = $('#question');
+	var $answerForm = $('#answer-form');
+	var $answerInput = $('#answer-input');
+
+	function newQuestion() {
+	}
+
+	newQuestion();
+
+});
